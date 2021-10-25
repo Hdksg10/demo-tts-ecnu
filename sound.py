6b8599010a6b67ddcb295261726c1e046a393c95
Wu MianZhi <31810920+Hdksg10@users.noreply.github.com>
2021-10-25 14:24:56 +0800

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,1 +1,11 @@
 import tts
+DrugDatabase = {
+    "6911322033949":'聚维酮碘溶液',
+    "6900233783173":'百令胶囊'
+}
+while True:
+    name = input()
+    try:
+        tts.say(DrugDatabase[name])
+    except KeyError:
+        tts.say("药品尚未录入")
